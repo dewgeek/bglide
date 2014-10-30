a9c8a48473aac0c32e81469b7a3423d36fadd7e5
rakesh godhala <mailme.rakeshg@gmail.com>
2014-10-31 00:39:02 +0530

    Update forms.py
    removed key

--- forms.py
+++ forms.py
@@ -6,4 +6,4 @@ from wtfrecaptcha.fields import RecaptchaField
 class contactForm(Form):
     email = StringField('Email')
     msg = TextAreaField('Message')
-    recaptcha = RecaptchaField(public_key="6LdUFPESAAAAAPxnU6Dd52HhUl9O63EcjOSKksFU", private_key="6LdUFPESAAAAADbGULOyQ46_UZ0JjyaRjrFFFqEy", secure=True)
+    recaptcha = RecaptchaField(public_key="xxx", private_key="xxx", secure=True)
